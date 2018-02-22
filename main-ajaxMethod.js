8f7d28b7277d7e3f1c0e4a1f879ed3b6c8a28b18
Mariana Souza <marianacaldassouza@gmail.com>
2018-02-22 12:26:07 -0800

    Adding new main.js with Ajax method

---
+++ main-ajaxMethod.js
@@ -0,0 +1,76 @@
+/ Include data for accessing Google APIs
+
+const apiKey = 'AIzaSyBKsPi8dOhaG0vphDtCsk6j-46iDN85cCU';
+const projection = 'FULL';
+const url = 'https://www.googleapis.com/urlshortener/v1/url';
+
+// Some page elements
+
+const $inputField = $('#input');
+const $expandButton = $('#expand');
+const $shortenButton = $('#shorten');
+const $responseField = $('#responseField');
+
+// AJAX functions
+
+function expandUrl() {
+ const  urlToExpand = url + '?key=' + apiKey +
+'&shortUrl=' + $inputField.val();
+ $.ajax({
+   url: urlToExpand,
+   type: 'GET',
+   dataType: 'json',
+   success(response){
+     $responseField.append('<p>Your expanded url is: </p><p>' + response.longUrl + '</p>');
+   },
+   error(jqXHR, status, errorThrown){
+     console.log(jqXHR);
+   }
+   
+ });
+}
+
+
+function shortenUrl() {
+ const urlWithKey = url + '?key=' + apiKey;
+ const urlToShorten = $inputField.val();
+ $.ajax({
+   url: urlWithKey,
+   type:'POST',
+   data: JSON.stringify({longUrl: urlToShorten}),
+   dataType: 'json',
+   contentType: 'application/json',
+   success(response){
+     $responseField.append('<p>Your shortened url is: </p><p>' + response.id + '</p>');
+   },
+   error(jqXHR, status, errorThrown){
+     console.log(jqXHR);
+   }
+   
+ });
+}
+
+function expand() {
+  $responseField.empty();
+  if (!$inputField.val()) { 
+    $responseField.append('<p>Enter a valid URL</p>');
+    return false; 
+  }  
+  expandUrl();
+  return false;
+}
+
+function shorten() {
+  $responseField.empty();
+  if (!$inputField.val()) { 
+    $responseField.append('<p>Enter a valid URL</p>');
+    return false; 
+  }  
+  shortenUrl();
+  return false;
+}
+
+// Call functions on submit
+
+$expandButton.click(expand);
+$shortenButton.click(shorten);
